6c61dfdfd7153b3291696ae855e00c0137ff8b4b
mrylraihan <mr.ylraihan@gmail.com>
2025-12-25 22:09:41 -0800

    Update

--- Section3/87/01-starting-project/app/page.js
+++ Section3/87/01-starting-project/app/page.js
@@ -1,15 +1,17 @@
 "use client";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+// import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
   console.log("test");
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
-      <button onClick={()=>router.push('/about')}>Go to About</button>
+      <Link href={'/about'}><p>About us!</p></Link>
+      {/* <button onClick={()=>router.push('/about')}>Go to About</button> */}
     </main>
   );
 }
