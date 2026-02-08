46ffbb40d5b955913ac158bd572ad85a3893f658
mrylraihan <mr.ylraihan@gmail.com>
2026-02-07 21:46:43 -0800

    Update

--- PersonalPractice/my-next-app8/app/page.js
+++ PersonalPractice/my-next-app8/app/page.js
@@ -1,13 +1,18 @@
 import styles from "./page.module.css";
-
+import bladeRunner from "../public/bladerunner.jpg";
+import Image from "next/image";
 export default function Home() {
+  console.log(process.env.SECRET_KEY)
   return (
+    <>
     <div className={styles.page}>
       <main className={styles.main}>
           <h1 className={styles.title}>
            Home Page
           </h1>
+          <Image src={bladeRunner} alt="Blade Runner" width={800} height={600} />
       </main>
     </div>
+    </>
   );
 }
