67dfbddb24bac098a0ef84f7b8c6b92f27a1b780
mrylraihan <mr.ylraihan@gmail.com>
2026-02-15 07:58:56 -0800

    Update loading with react

--- Old/Section2(ReactRefresher)/ReactPersonalPractice/PageToggle/my-react-app/src/App.jsx
+++ Old/Section2(ReactRefresher)/ReactPersonalPractice/PageToggle/my-react-app/src/App.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import Test1 from './components/Test1'
 import Test2 from './components/Test2'
 
+export const ShowBlogDetails=()=>{
+	const [data, setData] = useState({})
+	useEffect(()=>{
+		fetch("https://jsonplaceholder.typicode.com/posts/1")
+		.then(res=>res.json())
+		.then(data=>{
+			return setTimeout(()=>{
+				console.log("Timed Out")
+				setData(data)
+			},2000)
+		})
+	},[])
+	console.log(data)
+	if(data.title){
+		return <h1>{data.title}</h1>
+	}else{
+		return <h3>...Loading</h3>
+	}
+}
+
 function App() {
   const [toggle, setToggle] = useState(true)
   return (
-		<div>
-			{toggle ? (
-				<Test1 toggle={toggle} setToggle={setToggle} />
-			) : (
-				<Test2 toggle={toggle} setToggle={setToggle} />
-			)}
+	<>
+	<div>
+		<ShowBlogDetails/>
+	</div>
+	<div>
+		{toggle ? (
+			<Test1 toggle={toggle} setToggle={setToggle} />
+		) : (
+			<Test2 toggle={toggle} setToggle={setToggle} />
+		)}
     </div>
+	</>
 	)
 }
 
