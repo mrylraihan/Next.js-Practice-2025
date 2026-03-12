6111fe5948d5f3ad37c77f6ffdf7626f777ca7fa
mrylraihan <mr.ylraihan@gmail.com>
2026-03-11 23:08:32 -0700

    practice suspense

--- PersonalPractice/my-next-app8/app/contact/page.js
+++ PersonalPractice/my-next-app8/app/contact/page.js
@@ -1,12 +1,28 @@
 import styles from "../page.module.css";
 
-export default function Contact() {
+export default async function Contact() {
+  const res = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        name: "John Doe",
+        email: "john.doe@example.com",
+      });
+    }, 2000);
+  });
+
+  const data = await res; 
+
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
           <h1 className={styles.title}>
            Contact Page
           </h1>
+          <div>
+            <h2>{data.name}</h2>
+            <h2>{data.email}</h2>
+          </div>
       </main>
     </div>
   );
--- PersonalPractice/my-next-app8/app/services/page.js
+++ PersonalPractice/my-next-app8/app/services/page.js
@@ -1,6 +1,16 @@
+import { Suspense } from "react";
 import styles from "../page.module.css";
 
-export default function Services() {
+export async function ServicesData() {
+  const res = new Promise((res, rej)=>{
+    setTimeout(()=>{
+      res({
+        name: "John Doe",
+      })
+    }, 2000)
+  })
+  const data = await res;
+  console.log(data)
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -8,6 +18,17 @@ export default function Services() {
            Services Page
           </h1>
       </main>
+    </div>
+  );
+}
+
+export default function Services() {
+  
+  return (
+    <div className={styles.page}>
+      <Suspense fallback={<h2>Loading Services...</h2>}>
+      <ServicesData />
+      </Suspense>
     </div>
   );
 }
