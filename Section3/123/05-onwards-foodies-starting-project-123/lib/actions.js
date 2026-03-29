5ae7d4b55531b77de63829af7459d6e87cb7b6c4
Wayel N Raihan <mr.ylraihan@gmail.com>
2026-03-29 00:30:28 -0700

    Updated

--- Section3/123/05-onwards-foodies-starting-project-123/lib/actions.js
+++ Section3/123/05-onwards-foodies-starting-project-123/lib/actions.js
@@ -1,20 +1,34 @@
-'use server';
+'use server'
 
-import { redirect } from "next/navigation";
-import { saveMeal } from "./meals";
+import { redirect } from 'next/navigation'
+import { saveMeal } from './meals'
+
+const isInvalidTest = (text) => {
+	return !text || text.trim() === ''
+}
 
 export async function shareMeal(formData) {
-  setTimeout(() => {}
-  , 3000)
-  const mealData = {
-    creator: formData.get("name"),
-    creator_email: formData.get("email"),
-    title: formData.get("title"),
-    summary: formData.get("summary"),
-    instructions: formData.get("instructions"),
-    image: formData.get("image"),
-  }
-  console.log(mealData)
-  await saveMeal(mealData)
-  redirect('/meals')
-}       
+	setTimeout(() => {}, 3000)
+	const mealData = {
+		creator: formData.get('name'),
+		creator_email: formData.get('email'),
+		title: formData.get('title'),
+		summary: formData.get('summary'),
+		instructions: formData.get('instructions'),
+		image: formData.get('image'),
+	}
+	if (
+		isInvalidTest(mealData.title) ||
+		isInvalidTest(mealData.summary) ||
+		isInvalidTest(mealData.instructions) ||
+		isInvalidTest(mealData.creator) ||
+		isInvalidTest(mealData.creator_email) ||
+		!mealData.creator_email.includes('@') ||
+		mealData.image.size === 0
+	) {
+		throw new Error('Invalid meal data')
+	}
+	console.log(mealData)
+	await saveMeal(mealData)
+	redirect('/meals')
+}
