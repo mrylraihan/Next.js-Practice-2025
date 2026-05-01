841d07a43fe46b346fd38b7de3040058d8e69d10
Wayel N Raihan <mr.ylraihan@gmail.com>
2026-05-01 10:03:01 -0700

    Updated

--- PersonalPractice/my-next-app10/app/about/layout.jsx
+++ PersonalPractice/my-next-app10/app/about/layout.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from '../page.module.css'
 
-export default function About({ aboutteam, aboutprogram }) {
+export default function About({ children, aboutteam, aboutprogram }) {
 	return (
 		<div className={styles.page}>
 			<main className={styles.main}>
+				{children}
 				{aboutteam}
 				{aboutprogram}
 			</main>
